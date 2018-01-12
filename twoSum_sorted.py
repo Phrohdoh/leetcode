0570f69cad75ae0f83f9408320771cdcee46f4cf
Taryn Hill <taryn@phrohdoh.com>
2018-01-12 09:18:45 -0800

    Begin working on two sum with sorted input in python3

---
+++ twoSum_sorted.py
@@ -0,0 +1,11 @@
+from bisect import bisect
+
+class Solution:
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        # TODO: bisect?
